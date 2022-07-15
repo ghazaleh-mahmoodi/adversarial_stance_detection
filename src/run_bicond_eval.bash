1ae428f9f568aa22f89a5cea5b872d0a8228c9b7
Ghazaleh <gh_mhdi@outlook.com>
2022-07-15 09:38:31 +0000

    update bash file to read hyperparameter from new config

--- src/run_bicond_eval.bash
+++ src/run_bicond_eval.bash
@@ -5,7 +5,7 @@ do
     echo "$topic" 
     echo "test time"
     python3 train_and_eval_model.py --mode "eval" \
-    --config_file data/config_example_bicond.txt \
+    --config_file data/config_example_bicond_${topic}.txt \
     --trn_data data/twitter_test${topic}_seenval/train.csv \
     --dev_data data/twitter_test${topic}_seenval/test.csv \
     --saved_model_file_name data/checkpoints/ckp-BiCondLSTM-basic-twitter-example_${topic}-BEST.tar \
--- src/run_toad_eval.bash
+++ src/run_toad_eval.bash
@@ -5,7 +5,7 @@ do
     echo "$topic" 
     echo "test time"
     python3 train_and_eval_model.py --mode "eval" \
-    --config_file data/config_example_toad.txt \
+    --config_file data/config_example_toad_${topic}.txt \
     --trn_data data/twitter_test${topic}_seenval/train.csv \
     --dev_data data/twitter_test${topic}_seenval/test.csv \
     --saved_model_file_name data/checkpoints/ckp-BasicAdv-twitter-example-bicond_${topic}-BEST.tar \
