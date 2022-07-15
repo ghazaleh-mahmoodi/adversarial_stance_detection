0167cfd89aafdcb704f2894f98618ea5aae6d2aa
Ghazaleh <gh_mhdi@outlook.com>
2022-07-15 04:16:12 +0000

    remove extra file.

--- src/bicond.sh
+++
@@ -1,5 +0,0 @@
-train_data=data/twitter_testDT_seenval/development_setup/train.csv
-dev_data=data/twitter_testDT_seenval/development_setup/validation.csv
-
-echo "training bicond model on twitter data"
-python -u train_model.py -s $1 -i ${train_data} -d ${dev_data} -k $2
